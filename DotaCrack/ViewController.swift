5b85c9c31d3c27a83f96a79ef6726d13c3de8cbe
Nicholas Dauchot <NDauchot@Nicholass-MacBook-Air.local>
2015-04-21 18:56:42 -0400

    Initial Commit

---
+++ DotaCrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DotaCrack
+//
+//  Created by Nicholas Dauchot on 4/21/15.
+//  Copyright (c) 2015 blackhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
